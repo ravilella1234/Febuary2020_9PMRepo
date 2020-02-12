391915a31d1b7e8e4ca9cbf1b0c54d2629105781
maha <maha@gmail.com>
2020-02-12 14:18:30 +0530

    added p,q,r variables into Democalss from california Branch

--- Oracle/src/com/hdfc/loans/eduloans/DemoClass.java
+++ Oracle/src/com/hdfc/loans/eduloans/DemoClass.java
@@ -2,7 +2,8 @@ package com.hdfc.loans.eduloans;
 
 public class DemoClass 
 {
-
+	int p=1000,q=2000,r=3000;
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Added firstLine.....");
